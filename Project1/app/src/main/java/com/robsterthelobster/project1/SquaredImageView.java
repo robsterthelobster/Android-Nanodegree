faf5fbe25b82537de5bfe6f66803b04514e9816a
Robin Chen <robin3chen@gmail.com>
2016-03-21 23:40:54 -0700

    Picasso GridView Sample

---
+++ Project1/app/src/main/java/com/robsterthelobster/project1/SquaredImageView.java
@@ -0,0 +1,26 @@
+package com.robsterthelobster.project1;
+
+/**
+ * Created by robin on 3/21/2016.
+ * Based on picasso sample: https://github.com/square/picasso/
+ */
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/** An image view which always remains square with respect to its width. */
+final class SquaredImageView extends ImageView {
+    public SquaredImageView(Context context) {
+        super(context);
+    }
+
+    public SquaredImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+    }
+}
